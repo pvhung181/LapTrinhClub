13a80a3af57da26173a93a904290f1eae7ca6a9e
pv_hung <88100132+Hung7201@users.noreply.github.com>
2022-08-19 23:41:45 +0700

    Update dao_mang_ki_tu.cpp

--- dao_mang_ki_tu.cpp
+++ dao_mang_ki_tu.cpp
@@ -1,22 +1,26 @@
 #include<bits/stdc++.h>
 
 #define endl "\n"
+#define fi first
+#define se second
+#define forit(it,m) for(auto it : m)
+
 using namespace std;
 
-typedef unsigned long long ull;
 typedef long long ll;
-typedef long double ld;
 
-const double PI  = 3.141592653589793238463;
-const ll  mod = 1e9+7;
+inline ll _gcd(ll a, ll b) {ll r;while(b != 0) {r = a % b;a = b;b = r;}return a;}
+inline ll _lcm(ll a, ll b) {return (a * b) / _gcd(a,b);}
 
-int main()
-{
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    string str;
-    char c;
-    ll n ;
-    
+void inputArr(int start, int end, ll arr[]) {
+	for(int i = start; i < end; i++) cin >> arr[i];
 }
 
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int n; cin >> n; fflush(stdin);
+	string str; cin >> str;
+    reverse(str.begin(), str.end());
+    cout << str;
+}
 
