05fe5b63e49c8a8b57e3bb7a7640176282d1d445
pv_hung <88100132+Hung7201@users.noreply.github.com>
2022-11-03 22:14:28 +0700

    Update TimDienTichHinhChuNhatCoDienTichLonNhi.cpp

--- TimDienTichHinhChuNhatCoDienTichLonNhi.cpp
+++ TimDienTichHinhChuNhatCoDienTichLonNhi.cpp
@@ -1,29 +1,63 @@
 #include<bits/stdc++.h>
+
+#define endl "\n"
 using namespace std;
 
 typedef unsigned long long ull;
 typedef long long ll;
 typedef long double ld;
 
-const ll  M=1e9+7;
+const double PI  = 3.141592653589793238463;
+const ll  mod = 1e9+7;
+
+ll _gcd(ll a, ll b) {ll r;while(b != 0) {r = a % b;a = b;b = r;}return a;}
+ll _lcm(ll a, ll b) {return (a * b) / _gcd(a,b);}
+
+typedef struct hcn{
+   ll l, w, s;
+   bool  operator < (hcn other)
+   	{
+   		return s > other.s;
+	   }
+
+};
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n; cin>>n;
-    pair<ll,ll> c[n];
-    for(int i=0;i<n;i++)
+    int n; cin >> n;
+    hcn a[n];
+    for(int i = 0; i < n; i++)
     {
-    	ll a,b; cin>>a>>b;
-    	c[i].first = a;
-    	c[i].second = b;
+    	cin >> a[i].l >> a[i].w;
+    	a[i].s = a[i].l * a[i].w;
 	}
-	set<ll> dt;
-	for(int i=0;i<n;i++)
-		dt.insert(c[i].first*c[i].second);
-	for(auto it:dt)
-	   cout<<it<<' ';
+	sort(a,a + n);
+    int cnt = 0, j = 1;
+    while(j < n )
+    {
+    	int i = 0;
+    	if(a[i].s != a[j].s){
+    		cnt++;
+    		break;
+		}
+		j++;
+	}
+	if(cnt == 0) cout << "NO";
+	else
+	{
+		int max = INT_MIN;
+		for(int i = 0; i < n; i++)
+		{
+			if(a[i].s > max)
+			    max = a[i].s;
+		}
+		for(int i = 0; i < n; i++)
+		{
+			if(a[i].s < max)
+			{
+				cout << a[i].l << ' ' << a[i].w;
+				break;
+			}
+		}
+	}
 }
-
